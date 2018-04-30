d11d9c5d7a3a3deda47e5ba2b9843769b1e312ce
Yacoub Awimrin <yawimrin0017@stu.d214.org>
2018-04-30 11:44:00 -0500

    added name

--- Class Guide/AppDelegate.swift
+++ Class Guide/AppDelegate.swift
@@ -15,7 +15,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+// Yacoub
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
